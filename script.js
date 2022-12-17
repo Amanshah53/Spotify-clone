180c260099321b353685556a1a8c564c27a425bd
Amanshah53 <amangup53@gmail.com>
2022-12-17 22:08:58 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+console.log("Welcome to Spotify");
+
+//Initialize the variables
+let songIndex = 0;
+let audioElement = new Audio('1.mp3');
+let masterPlay = document.getElementById('masterPlay');
+let myProgressBar = document.getElementById('myProgressBar');
+
+
+let songs = [
+    {songName: "Salam-e-Ishq", filepath: "songs/1.mp3", coverPath:"covers/1.jpg"},
+    {songName: "Salam-e-Ishq", filepath: "songs/1.mp3", coverPath:"covers/1.jpg"},
+    {songName: "Salam-e-Ishq", filepath: "songs/1.mp3", coverPath:"covers/1.jpg"},
+    {songName: "Salam-e-Ishq", filepath: "songs/1.mp3", coverPath:"covers/1.jpg"},
+    {songName: "Salam-e-Ishq", filepath: "songs/1.mp3", coverPath:"covers/1.jpg"},
+    {songName: "Salam-e-Ishq", filepath: "songs/1.mp3", coverPath:"covers/1.jpg"},
+]
+
+// audioElement.play();
+
+// Handle play/pause click
+masterPlay.addEventListener('click', ()=>{
+    if(audioElement.paused || audioElement.currentTime<=0){
+        audioElement.play();
+    }
+})
+//Listen to Events
+myProgressBar.addEventListener('timeupdate', () =>{
+    console.log('timeupdate');
+    //update seek bar
+})
